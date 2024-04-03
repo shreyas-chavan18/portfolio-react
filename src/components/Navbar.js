fe2e96fe9f7fcc10a72bac3b923849c4c9c1494a
shreyas-chavan18 <131510722+shreyas-chavan18@users.noreply.github.com>
2024-04-03 17:49:13 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/shreyas-chavan18/portfolio-react"
                 target="_blank"
                 className="fork-btn-inner"
               >
